d0628badcf9da15b5ed7e5ffaa70a0e1c1f17613
TW <thilo.wilts@iu-study.org>
2025-02-20 09:04:31 +0100

    new module

---
+++ my_module2.py
@@ -0,0 +1,6 @@
+# my_module.py
+def greet(name):
+    return f"Hello, {name}!"
+
+def sub(a, b):
+    return a - b
--- ui2.py
+++ ui2.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import my_module  # Importing the module
+import my_module2 # Importing the module2
 
 # Using functions from the module
 # new comment
 print(my_module.greet("Alice"))
+
+print(my_module2.greet("Bob"))
