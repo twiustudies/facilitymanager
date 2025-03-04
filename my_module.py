96dafb488d678296f77c6e632d485401dfb596cf
TW <thilo.wilts@iu-study.org>
2025-03-04 15:17:06 +0100

    deleted test files

--- my_module.py
+++
@@ -1,6 +0,0 @@
-# my_module.py
-def greet(name):
-    return f"Hello, {name}!"
-
-def add(a, b):
-    return a + b
--- my_module2.py
+++
@@ -1,6 +0,0 @@
-# my_module.py
-def greet(name):
-    return f"Hello, {name}!"
-
-def sub(a, b):
-    return a - b
